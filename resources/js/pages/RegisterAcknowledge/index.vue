59534df422d7171f557f50974aa11a20924f6465
Bob Fix <bobfix@gmail.com>
2019-05-21 20:16:06 -0500

    Add registration acknowledgment page

---
+++ resources/js/pages/RegisterAcknowledge/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <nav-bar></nav-bar>
+        <div class="content">
+            <div class="title">
+                {{ title }}
+            </div>
+            <div class="subtitle">
+                <p>Please check your e-mail for an entry confirmation.</p>
+                <p>We look forward to seeing you at the 41st Annual Litchfield Watercade Tennis Tournament!</p>
+            </div>
+            <div class="sponsored_by m-b-md">
+                <p>Please <a href="mailto:watercadetennis@gmail.com?subject=Litchfield%20Watercade%20Registration">email</a>
+                    us if you have any questions.</p>
+            </div>
+        </div>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import NavBar from "../../components/NavBar";
+    import AppFooter from "../../components/AppFooter";
+    export default {
+        name: 'RegisterAcknowledge',
+        components: {
+            AppFooter,
+            NavBar
+        },
+        data: function() {
+            return {
+                title: 'Thank You!',
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .content {
+        min-height: calc(100vh - 200px);
+    }
+</style>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,7 +1,8 @@
 import NotFound from "./pages/404/index";
 import Home from "./pages/Home/index";
 import Info from "./pages/Info/index";
-import Register from "./pages/Register/index"
+import Register from "./pages/Register/index";
+import RegisterAcknowledge from "./pages/RegisterAcknowledge/index";
 
 export default {
     mode: 'history',
@@ -26,6 +27,11 @@ export default {
             path: '/register',
             name: 'register',
             component: Register
+        },
+        {
+            path: '/register-acknowledge',
+            name: 'register-acknowledge',
+            component: RegisterAcknowledge
         }
     ]
 };
